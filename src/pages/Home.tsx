b0ed1c53d37511dc9f86d963a32ec4e75e2bcbc3
Renan <renan@pop-os.localdomain>
2022-02-08 21:42:42 -0300

    Implementa desafio

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -9,15 +9,32 @@ export function Home() {
   const [tasks, setTasks] = useState<Task[]>([]);
 
   function handleAddTask(newTaskTitle: string) {
-    //TODO - add new task
+    const newTask = {
+      id: new Date().getTime(),
+      title: newTaskTitle,
+      done: false
+    }
+    setTasks(oldTasks => [...oldTasks, newTask])
   }
 
   function handleToggleTaskDone(id: number) {
-    //TODO - toggle task done if exists
+    const updateTasks = [...tasks];
+
+    const findTask = updateTasks.find(task => task.id === id);
+
+    if (!findTask)
+      return;
+
+    findTask.done = !findTask.done;
+
+    setTasks(updateTasks);
   }
 
   function handleRemoveTask(id: number) {
-    //TODO - remove task from state
+    setTasks(oldTasks => oldTasks.filter(
+      task => task.id !== id
+    ));
+
   }
 
   return (
@@ -26,10 +43,10 @@ export function Home() {
 
       <TodoInput addTask={handleAddTask} />
 
-      <TasksList 
-        tasks={tasks} 
+      <TasksList
+        tasks={tasks}
         toggleTaskDone={handleToggleTaskDone}
-        removeTask={handleRemoveTask} 
+        removeTask={handleRemoveTask}
       />
     </View>
   )
